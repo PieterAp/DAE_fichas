1cbe063d3fbf3ab9e31fa8df4f0c795599a3886e
Pieter <2191763@my.ipleiria.pt>
2023-10-14 18:21:27 +0100

    ficha 2, part II

---
+++ academics-client/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-if="error">
+    Error: {{ error.message }}
+  </div>
+  <div v-else>
+    <h2>Students</h2>
+    <div>{{ students }}</div>
+    <button @click.prevent="refresh">Refresh Data</button>
+  </div>
+</template>
+<script setup>
+  const config = useRuntimeConfig();
+  const api = config.public.API_URL;
+  const { data: students, error, refresh } = await useFetch(`${api}/students`)
+</script>
